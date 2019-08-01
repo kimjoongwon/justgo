6d528cf79912ee13a294b9bf6657a44e8e469dbd
wally <kimjwally@gmail.com>
2019-08-01 10:39:11 +0900

    dad

--- imports/api/users/server/publications.jsx
+++ imports/api/users/server/publications.jsx
@@ -1,13 +1,16 @@
-import { Meteor } from "meteor/meteor";
+import { Meteor } from 'meteor/meteor';
 
-
-
-Meteor.publish("users", function() {
-  return Meteor.users.find();
+Meteor.publish('users', function() {
+	return Meteor.users.find();
 });
+
+Meteor.publish('user', function() {
+	return Meteor.user();
+});
+
 // Meteor.publish("currentuser", function() {
 //   return Meteor.user();
 // });
-Meteor.publish("userStatus", function() {
-  return Meteor.users.find({ "status.online": true });
+Meteor.publish('userStatus', function() {
+	return Meteor.users.find({ 'status.online': true });
 });
